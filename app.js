64066bb93124855fbc3e8cd27685e0a09949e83b
yara <76455290+yara1611@users.noreply.github.com>
2023-09-23 10:12:12 +0300

    added read books counter and sorting function

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const addBtn = document.querySelector('#AddBook')
 const remBtn = document.querySelector('#RemBook')
 const bookList = document.querySelector('.bookList')
 const counterBook = document.querySelector('#counter')
+const counterRead = document.querySelector('#Rcounter')
+const sort = document.body.querySelector("#sort")
+
 //inputs
 const inputs = document.querySelectorAll('input')
 const title = inputs[0]
@@ -46,7 +49,7 @@ Book.prototype.toggleRead = function(){
 
 function toggleRead(index) {
     myLibrary[index].toggleRead()
-    display()
+    display(myLibrary)
 }
 
 book1=new Book ("Once Upon a Broken Heart","Stephanie Garber",416,2021,1)
@@ -56,20 +59,20 @@ book3 =new Book ("A Curse for True Love","Stephanie Garber",400,2023,0)
 myLibrary.push(book1)
 myLibrary.push(book2)
 myLibrary.push(book3)
-display()
+display(myLibrary)
 
 addBtn.addEventListener('click',addBookToLibrary)
 function addBookToLibrary() {
     book = new Book(title.value,auth.value,pages.value,year.value)
     myLibrary.push(book)
-    console.log(myLibrary)
-    display()
+    // console.log(myLibrary)
+    display(myLibrary)
 }
 
-function display(){
+function display(myLibrary){
     bookList.innerHTML=''
     for(let i=0;i<myLibrary.length;i++){
-        console.log(myLibrary[i])
+        // console.log(myLibrary[i])
         item = document.createElement('div')
         item.classList.add('bookItem')
         item.id=i
@@ -79,17 +82,37 @@ function display(){
         <br>Pages: ${myLibrary[i].pages}
         <br>Year: ${myLibrary[i].year}
         <p>${myLibrary[i].read? "Read":"Not Read Yet"}</p>
-        <button  type'button' onclick="toggleRead(${i})">Read</button>
-        <button type="button" onclick='removeBook(${i})'> Remove</button><br>
+        <button type'button' onclick="toggleRead(${i})">Read</button>
+        <button type="button" onclick='removeBook(${i})'>Remove</button><br>
         `
         bookList.append(item)
     }
+    
     counterBook.innerText = `Book Count: ${myLibrary.length}`
+    counterRead.innerText = `Books Read Count: ${myLibrary.filter((a=>a.read==1)).length}`
+    
 }
+
+   
+// function strip(myLibrary) {
+//     return myLibrary.replace(/^(a |the |an )/i, '').trim();
+//   }   
+
+function sortBooks(t) {
+    if(t==='Asc')
+        display(myLibrary.sort((a,b)=>a.title>b.title?1:-1))
+    else
+        display(myLibrary.sort((a,b)=>a.title<b.title?1:-1))
+}
+
+ sort.addEventListener('change', function(e){
+   sortBooks(e.target.value)
+ })
+
 
 function removeBook(index){
     myLibrary.splice(index,1)
-    console.log(myLibrary)
-    display()
+    // console.log(myLibrary)
+    display(myLibrary)
 }
 
